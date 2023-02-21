9e531e5acb7224ddb721ab8e9ce853c209117f17
krwjang <38675309+krwjang@users.noreply.github.com>
2023-02-21 13:22:51 +0900

    Update 삼성전자우.py

--- 삼성전자우.py
+++ 삼성전자우.py
@@ -24,9 +24,11 @@ def read_price(preferred, common, start, end):
     우선주티커, 본주티커, 불러오기 시작일자
     '''
     preferred = fdr.DataReader(preferred, start=start, end=end)[['Open', 'High', 'Low', 'Close']]
+    preferred["OHLC_avg"] = (ratio.Open + ratio.High + ratio.Low + ratio.Close) / 4
     common = fdr.DataReader(common, start=start, end=end)[['Open', 'High', 'Low', 'Close']]
+    common["OHLC_avg"] = (ratio.Open + ratio.High + ratio.Low + ratio.Close) / 4
+
     ratio = preferred / common
-    ratio["OHLC_avg"] = (ratio.Open + ratio.High + ratio.Low + ratio.Close) / 4
 
     return ratio
 
