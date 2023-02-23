5d60bf619a47f39c4fdcc8cf36ddca14e9b66384
krwjang <38675309+krwjang@users.noreply.github.com>
2023-02-23 17:01:50 +0900

    Update 삼성전자우.py

--- 삼성전자우.py
+++ 삼성전자우.py
@@ -114,8 +114,8 @@ x120 = df.tail(120)["OHLC_avg"]
 x250 = df.tail(250)["OHLC_avg"]
 
 fig_sum = go.Figure()
-fig_sum.add_trace(go.Histogram(x=x250, name="1년"))
-fig_sum.add_trace(go.Histogram(x=x120, name="반기"))
+fig_sum.add_trace(go.Histogram(x=x250, name="1년", color = "red"))
+fig_sum.add_trace(go.Histogram(x=x120, name="반기", color = "orenge))
 fig_sum.add_trace(go.Histogram(x=x60, name="분기"))
 fig_sum.add_trace(go.Histogram(x=x20, name="1달"))
 
